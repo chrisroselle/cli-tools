db883cff94287034540fc6f39becc25fa63cc3b2
Chris Roselle <chrisroselle@gmail.com>
2021-07-12 02:08:32 -0400

    add script for generating skeleton code using getopt

---
+++ shell/script/generate_getopt/generate_getopt.py
@@ -0,0 +1,270 @@
+import argparse
+from enum import Enum
+
+import yaml
+
+
+class GetoptType(Enum):
+    FUNCTION = 'function'
+    SCRIPT = 'script'
+
+
+class Option:
+    def __init__(self, d):
+        # Parse Inputs
+        if 'long' not in d:
+            raise Exception('"long" is required for all options')
+        self.long = d['long']
+        self.short = None
+        if 'short' in d:
+            self.short = d['short']
+        self.flag = False
+        if 'flag' in d:
+            self.flag = bool(d['flag'])
+        self.required = False
+        if 'required' in d:
+            self.required = bool(d['required'])
+        self.description = ''
+        if 'description' in d:
+            self.description = d['description'].replace('$', '\\$')
+        self.default = None
+        if 'default' in d:
+            self.default = d['default']
+
+        # calculate other fields
+        self.variable = self.long.replace('-', '_')
+        if self.short:
+            self.getopt_short = f'{self.short}'
+        self.getopt_long = f'{self.long}'
+        if self.flag and self.default is True:
+            self.getopt_long = f'no-{self.long}'
+        if not self.flag:
+            self.getopt_short += ":"
+            self.getopt_long += ":"
+        if self.flag and self.default is True:
+            self.ustring = f'--no-{self.long}'
+        else:
+            self.ustring = f'--{self.long}'
+        self.case = self.ustring
+        self.ustring_value = f'{self.long.upper()}'
+        if self.default:
+            self.ustring_value = self.default
+        if self.short:
+            self.ustring = f'-{self.short},{self.ustring}'
+            self.case = f'-{self.short}|{self.case}'
+        if not self.flag:
+            self.ustring += f'={self.ustring_value}'
+        del self.ustring_value
+        if not self.required:
+            self.ustring = f'[{self.ustring}]'
+        if self.description:
+            self.usage = (f'  {self.ustring}', self.description)
+        else:
+            self.usage = (f'  {self.ustring}', '')
+        if self.flag:
+            if self.default is True:
+                self.case = f'{self.case}) {self.variable}="false"; shift ;;'
+            else:
+                self.case = f'{self.case}) {self.variable}="true"; shift ;;'
+        else:
+            self.case = f'{self.case}) {self.variable}="$2"; shift 2 ;;'
+
+        del self.ustring
+
+
+class Positional:
+    def __init__(self, d):
+        # Parse Inputs
+        if 'long' not in d:
+            raise Exception('"long" is required for positional parameters')
+        self.long = d['long']
+        self.required = True
+        if 'required' in d:
+            self.required = bool(d['required'])
+        self.multiple = False
+        if 'multiple_allowed' in d:
+            self.multiple = bool(d['multiple_allowed'])
+        self.description = ''
+        if 'description' in d:
+            self.description = d['description']
+
+        # calculate other fields
+        self.variable = self.long.replace('-', '_')
+        self.ustring = self.long.upper()
+        if self.required:
+            if self.multiple:
+                self.usage = f' {self.ustring} [{self.ustring} ...]'
+            else:
+                self.usage = f' {self.ustring}'
+        else:
+            if self.multiple:
+                self.usage = f' [{self.ustring} {self.ustring} ...]'
+            else:
+                self.usage = f' [{self.ustring}]'
+
+
+parser = argparse.ArgumentParser(
+    description='Tool for generating skeleton code for a script or function which uses getopt to parse inputs'
+)
+parser.add_argument('configuration', help='The configuration file that specifies the inputs, in YAML format')
+args = parser.parse_args()
+
+# getopt parameters
+short_options = ""
+long_options = []
+
+# what will go in usage output
+positional_usage = []
+required_option_usage = []
+optional_option_usage = []
+
+# bash variables
+variables = []
+variables_with_no_default = []
+variables_with_defaults = {}
+
+case_statements = []
+validation_statements = []
+
+
+def p(o, indent=0):
+    for line in o.split('\n'):
+        print(' ' * (indent * 4) + line)
+
+
+def usage_statement():
+    if getopt_type is GetoptType.SCRIPT:
+        return 'usage'
+    if getopt_type is GetoptType.FUNCTION:
+        return f'{{ {usage_name}; return 1; }}'
+
+
+with open(args.configuration, 'r') as cfg_yaml:
+    cfg = yaml.safe_load(cfg_yaml)
+
+try:
+    getopt_type = GetoptType(cfg['type'])
+except ValueError:
+    raise Exception(f'"{cfg["type"]}" is not a valid type - valid types are: {" ".join([x.value for x in GetoptType])}')
+
+name = cfg['name']
+if getopt_type is GetoptType.SCRIPT and not name.endswith('.sh'):
+    name += '.sh'
+
+try:
+    description = cfg['description']
+except KeyError:
+    description = 'This is a placeholder description'
+
+# usage function name
+if getopt_type is GetoptType.FUNCTION:
+    usage_name = f'_{name}_usage'
+    main_name = f'{name}'
+if getopt_type is GetoptType.SCRIPT:
+    usage_name = 'usage'
+    main_name = 'main'
+
+positional_usage_string = ''
+positional = None
+if 'positional' in cfg:
+    positional = Positional(cfg['positional'])
+    positional_usage_string = positional.usage
+    positional_usage.append(('  ' + positional.long.upper(), positional.description))
+    if positional.required:
+        if positional.multiple:
+            validation_statements.append(f'[[ -z "$1" ]] && {usage_statement()}')
+        else:
+            validation_statements.append(f'[[ -z ${positional.variable} ]] && {usage_statement()}')
+
+
+
+# TODO use case for no options?
+for o in cfg['options']:
+    opt = Option(o)
+    variables.append(opt.variable)
+
+    if opt.short:
+        short_options += opt.getopt_short
+
+    case_statements.append(opt.case)
+    long_options.append(opt.getopt_long)
+
+    if opt.required:
+        validation_statements.append(f'[[ -z ${opt.variable} ]] && {usage_statement()}')
+        required_option_usage.append(opt.usage)
+    else:
+        optional_option_usage.append(opt.usage)
+
+    if opt.default is not None:
+        if type(opt.default) is bool:
+            variables_with_defaults[opt.variable] = str(opt.default).lower()
+        else:
+            variables_with_defaults[opt.variable] = opt.default
+    else:
+        variables_with_no_default.append(opt.variable)
+
+# max option length for usage text
+mx = 0
+for u in required_option_usage + optional_option_usage + positional_usage:
+    mx = max(len(u[0]), mx)
+mx += 3
+
+# print it
+newline = '\n'
+print(f'''{usage_name}() {{
+    echo "usage: {name} [OPTIONS]{positional_usage_string}
+{newline.join([f'{u[0].ljust(mx)}{u[1]}' for u in positional_usage])}
+{newline.join([f'{u[0].ljust(mx)}{u[1]}' for u in required_option_usage])}
+{newline.join([f'{u[0].ljust(mx)}{u[1]}' for u in optional_option_usage])}
+
+{description}
+
+Examples:
+    {name}
+
+See Also:
+    reference" >&2''')
+if getopt_type is GetoptType.SCRIPT:
+    print('    exit 1')
+print('}\n')
+
+
+print(f'''{main_name}()  {{
+    # Input Parsing
+    local opts
+    opts=$(getopt --options "{short_options}" --longoptions "{",".join(long_options)}" -- "$@")
+    [[ $? != "0" ]] && {usage_statement()}
+    eval set -- "$opts"''')
+if len(variables_with_no_default) > 0:
+    print(f'    local {" ".join(sorted(variables_with_no_default))}')
+if len(variables_with_defaults) > 0:
+    for v in sorted(variables_with_defaults.keys()):
+        print(f'    local {v}="{variables_with_defaults[v]}"')
+print('''    while :; do
+        case "$1" in''')
+for c in case_statements:
+    print(f'            {c}')
+print(f'''            --) shift; break ;;
+            *) {usage_statement()} ;;
+        esac
+    done''')
+if positional and not positional.multiple:
+    print(f'    local {positional.variable}="$1"')
+
+print('''
+    # Input Validation''')
+for v in validation_statements:
+    print(f'    {v}')
+print('\n    # Function')
+if positional and positional.multiple:
+    print(f'''    local {positional.variable}
+    for {positional.variable} in "$@"; do
+        implement_me
+    done
+}}''')
+else:
+    print('''    implement_me
+}''')
+
+if getopt_type is GetoptType.SCRIPT:
+    print('main "$@"')
---
+++ shell/script/generate_getopt/test/test.sh
@@ -0,0 +1,252 @@
+#!/bin/bash
+
+fail() {
+    local message="$1"
+    shift
+    echo "-----------
+failure for input '$@'" >&2
+    echo "$message" >&2
+    rcode=$((rcode + 1))
+}
+
+testf_pass() {
+    local expected="$1"
+    shift
+    (set -o posix; set | egrep -v "^rcode") > env.before
+    "$@" 2>/dev/null >result.txt || { fail "expected function to run but it did not" "$@"; rm env.before result.txt; return; }
+    [[ $(cat result.txt) != $expected ]] && fail "actual output did not match expected
+actual:
+$result
+
+expected:
+$expected
+" "$@"
+    (set -o posix; set | egrep -v "^rcode") > env.after
+    diff -u <(cat env.before) <(cat env.after) || fail "environment changed after execution" "$@"
+    rm env.before env.after result.txt
+}
+
+testf_fail() {
+    "$@" 2>/dev/null && fail "expected function to fail but it did not" "$@"
+}
+
+rcode=0
+
+python -m venv test-venv
+source test-venv/bin/activate
+pip install -r ../requirements.txt
+
+python ../generate_getopt.py ./function.yaml > tmp.sh
+sed -i '/implement_me/a \
+    echo "example_flag = '\''$example_flag'\''" \
+    echo "other_flag = '\''$other_flag'\''" \
+    echo "thing = '\''$thing'\''" \
+    echo "widget = '\''$widget'\''" \
+    echo "input = '\''$input'\''"' tmp.sh
+
+sed -i '/implement_me/d' tmp.sh
+
+source tmp.sh
+testf_fail example_function
+testf_fail example_function -w testwidget
+testf_fail example_function --widget testwidget
+testf_fail example_function testinput
+
+expected="example_flag = 'false'
+other_flag = 'true'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" example_function --thing testthing --widget testwidget testinput
+
+expected="example_flag = 'true'
+other_flag = 'true'
+thing = 'value'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" example_function --example-flag -w testwidget testinput
+
+expected="example_flag = 'false'
+other_flag = 'false'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" example_function -z -t testthing -w testwidget testinput
+
+expected="example_flag = 'true'
+other_flag = 'false'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" example_function -f --no-other-flag --thing testthing --widget testwidget testinput
+
+expected="example_flag = 'true'
+other_flag = 'false'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" example_function -f -z --thing testthing --widget testwidget testinput testinput2
+
+python ../generate_getopt.py ./function2.yaml > tmp.sh
+sed -i '/^    for/i \
+    echo "example_flag = '\''$example_flag'\''" \
+    echo "other_flag = '\''$other_flag'\''" \
+    echo "thing = '\''$thing'\''" \
+    echo "widget = '\''$widget'\''"' tmp.sh
+
+sed -i '/implement_me/a \
+    echo "input = '\''$input'\''"' tmp.sh
+
+sed -i '/implement_me/d' tmp.sh
+source tmp.sh
+
+testf_fail example_function2
+testf_fail example_function2 -w testwidget
+testf_fail example_function2 --widget testwidget
+testf_fail example_function2 testinput
+
+expected="example_flag = 'false'
+other_flag = 'true'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" example_function2 --thing testthing --widget testwidget testinput
+
+expected="example_flag = 'true'
+other_flag = 'true'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" example_function2 --example-flag -t testthing -w testwidget testinput
+
+expected="example_flag = 'false'
+other_flag = 'false'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" example_function2 -z -t testthing -w testwidget testinput
+
+expected="example_flag = 'true'
+other_flag = 'false'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" example_function2 -f --no-other-flag --thing testthing --widget testwidget testinput
+
+expected="example_flag = 'false'
+other_flag = 'true'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'
+input = 'testinput2'"
+testf_pass "$expected" example_function2 --thing testthing --widget testwidget testinput testinput2
+
+python ../generate_getopt.py ./script.yaml > tmp.sh
+sed -i '/implement_me/a \
+    echo "example_flag = '\''$example_flag'\''" \
+    echo "other_flag = '\''$other_flag'\''" \
+    echo "thing = '\''$thing'\''" \
+    echo "widget = '\''$widget'\''" \
+    echo "input = '\''$input'\''"' tmp.sh
+
+sed -i '/implement_me/d' tmp.sh
+
+testf_fail ./tmp.sh
+testf_fail ./tmp.sh -w testwidget
+testf_fail ./tmp.sh --widget testwidget
+testf_fail ./tmp.sh testinput
+
+expected="example_flag = 'false'
+other_flag = 'true'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" ./tmp.sh --thing testthing --widget testwidget testinput
+
+expected="example_flag = 'true'
+other_flag = 'true'
+thing = 'value'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" ./tmp.sh --example-flag -w testwidget testinput
+
+expected="example_flag = 'false'
+other_flag = 'false'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" ./tmp.sh -z -t testthing -w testwidget testinput
+
+expected="example_flag = 'true'
+other_flag = 'false'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" ./tmp.sh -f --no-other-flag --thing testthing --widget testwidget testinput
+
+expected="example_flag = 'true'
+other_flag = 'false'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" ./tmp.sh -f -z --thing testthing --widget testwidget testinput testinput2
+
+python ../generate_getopt.py ./script2.yaml > tmp.sh
+sed -i '/^    for/i \
+    echo "example_flag = '\''$example_flag'\''" \
+    echo "other_flag = '\''$other_flag'\''" \
+    echo "thing = '\''$thing'\''" \
+    echo "widget = '\''$widget'\''"' tmp.sh
+
+sed -i '/implement_me/a \
+    echo "input = '\''$input'\''"' tmp.sh
+
+sed -i '/implement_me/d' tmp.sh
+
+testf_fail ./tmp.sh
+testf_fail ./tmp.sh -w testwidget
+testf_fail ./tmp.sh --widget testwidget
+testf_fail ./tmp.sh testinput
+
+expected="example_flag = 'false'
+other_flag = 'true'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" ./tmp.sh --thing testthing --widget testwidget testinput
+
+expected="example_flag = 'true'
+other_flag = 'true'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" ./tmp.sh --example-flag -t testthing -w testwidget testinput
+
+expected="example_flag = 'false'
+other_flag = 'false'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" ./tmp.sh -z -t testthing -w testwidget testinput
+
+expected="example_flag = 'true'
+other_flag = 'false'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'"
+testf_pass "$expected" ./tmp.sh -f --no-other-flag --thing testthing --widget testwidget testinput
+
+expected="example_flag = 'false'
+other_flag = 'true'
+thing = 'testthing'
+widget = 'testwidget'
+input = 'testinput'
+input = 'testinput2'"
+testf_pass "$expected" ./tmp.sh --thing testthing --widget testwidget testinput testinput2
+
+rm tmp.sh
+rm -r test-venv
+
+(( rcode > 0 )) && echo "$rcode test cases failed" >&2
+(( rcode == 0 )) && echo "all test cases passed"
+exit $rcode
