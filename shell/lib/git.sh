b5633c4af841cffd3041272eacfe8a3714ff0ace
Chris Roselle <chrisroselle@gmail.com>
2021-09-29 06:54:57 -0400

    wip

--- shell/lib/git.sh
+++ shell/lib/git.sh
@@ -1,3 +1,12 @@
+_g_is_clean() {
+    local status
+    status=$(git status --short)
+    if [[ -z $status ]]; then
+        return 0
+    fi
+    return 1
+}
+
 _g_sanity() {
     git status >/dev/null 2>&1 || {
         echo "not in a git repository" >&2
@@ -17,21 +26,19 @@ _g_default_branch() {
 }
 
 g_feature() {
-    _g_sanity || return 1
     local repo="$1"
     local new_branch="$2"
     [[ -z $new_branch ]] && {
         echo "usage: g_feature <repo> <new_branch_name>" >&2
         return 1
     }
-    cd ~/.git/$repo || return 1
+    cd $git/$repo || return 1
+    _g_sanity || return 1
     local current_branch default_branch
     current_branch=$(git branch --show-current)
     default_branch=$(_g_default_branch)
     if [[ $current_branch != $default_branch ]]; then
-        local status
-        status=$(git status --short)
-        if [[ -z $status ]]; then
+        if _g_is_clean; then
             git checkout "$default_branch"
         else
             git status --short
@@ -41,6 +48,17 @@ g_feature() {
     fi
     git pull
     git checkout -b "$new_branch" || return 1
+}
+
+g_pr_feature() {
+    _g_sanity || return 1
+    if ! _g_is_clean; then
+        git status --short
+        echo "error: working tree is unclean" >&2
+        return 1
+    fi
+    git push --set-upstream origin
+    gh pr create --title "$(git branch --show-current)" --body ""
 }
 
 #g_wip() {
