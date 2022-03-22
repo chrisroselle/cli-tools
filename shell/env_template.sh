0717f3028e54db568216a432c3c784a95b1f0662
Chris Roselle <chrisroselle@gmail.com>
2022-03-22 01:53:26 -0400

    don't assume we are root

--- shell/env_template.sh
+++ shell/env_template.sh
@@ -7,4 +7,4 @@ export HISTFILESIZE=99999
 export HISTIGNORE="exit"
 export HISTCONTROL="erasedups"
 
-python /cli-tools/shell/scripts/dedup.py /root/.bash_history
+python /cli-tools/shell/scripts/dedup.py ~/.bash_history
