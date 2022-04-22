23e37e8136d37894aed5887a17cf6a4788d35167
Chris Roselle <chrisroselle@gmail.com>
2022-04-22 07:15:05 -0400

    add update functions

--- shell/lib/wsl.sh
+++ shell/lib/wsl.sh
@@ -1,3 +1,15 @@
+wsl_patch() {
+    sudo dnf -y update
+    sudo pip install --upgrade pip
+    update_yq
+    update_kubectl
+    update_helm
+    update_node
+    update_maven
+    update_codefresh
+    update_pulumi
+}
+
 np() {
     "/mnt/c/Program Files (x86)/Notepad++/notepad++.exe" $@
 }
@@ -28,3 +40,94 @@ wsl_configs() {
         cp "$wdir/.cfconfig" ~/.cfconfig
     fi
 }
+
+update_yq() {
+    sudo curl -LJs $(curl -s https://api.github.com/repos/mikefarah/yq/releases/latest | grep browser_download_url | grep linux_amd64 | cut -d '"' -f 4) -o /usr/local/bin/yq
+    sudo chmod +x /usr/local/bin/yq
+}
+
+update_kubectl() {
+    sudo curl -sL "https://dl.k8s.io/release/$(curl -sL https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl \
+        && sudo chmod +x /usr/local/bin/kubectl
+}
+
+update_helm() {
+    (
+        mkdir /tmp/helm \
+            && cd /tmp/helm \
+            && HELM_VERSION=$(curl -s https://api.github.com/repos/helm/helm/releases/latest | jq -r '.tag_name') \
+            && curl -LJs "https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz" -o "helm.tar.gz" \
+            && tar xzf helm.tar.gz \
+            && sudo mv linux-amd64/helm /usr/local/bin/helm \
+            && sudo chmod +x /usr/local/bin/helm
+    )
+    rm -rf /tmp/helm
+}
+
+update_node() {
+    (
+        mkdir /tmp/node \
+        && cd /tmp/node \
+        && NODEJS_FILE=$(curl -s https://nodejs.org/download/release/latest-v14.x/ | grep "linux-x64.tar.xz" | cut -f2 -d '>' | cut -f1 -d '<') \
+        && curl -Ls "https://nodejs.org/download/release/latest-v14.x/${NODEJS_FILE}" -o "nodejs.tar.xz" \
+        && sudo tar -xJf "nodejs.tar.xz" -C /usr/local --strip-components=1 --no-same-owner \
+        && sudo rm /usr/local/CHANGELOG.md /usr/local/LICENSE /usr/local/README.md nodejs.tar.xz \
+        && sudo npm install --global yarn
+    )
+    rm -rf /tmp/node
+}
+
+update_maven() {
+    local MAVEN_VERSION=$(curl -s https://apache.osuosl.org/maven/maven-3/ | grep "<img" | tail -n 1 | cut -f3 -d '>' | cut -f1 -d '/')
+    [[ -d /usr/local/apache-maven-${MAVEN_VERSION} ]] && return 0
+    (
+        mkdir /tmp/maven \
+            && cd /tmp/maven \
+            && curl -s "https://apache.osuosl.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz" -o "maven.tar.gz" \
+            && sudo tar xzf maven.tar.gz -C /usr/local \
+            && sudo ln -s /usr/local/apache-maven-${MAVEN_VERSION}/bin/mvn /usr/local/bin/mvn \
+            && sudo ln -s /usr/local/apache-maven-${MAVEN_VERSION}/bin/mvnDebug /usr/local/bin/mvnDebug \
+            && sudo ln -s /usr/local/apache-maven-${MAVEN_VERSION}/bin/mvnyjp /usr/local/bin/mvnyjp
+    )
+    rm -rf /tmp/maven
+}
+
+update_codefresh() {
+    (
+        mkdir /tmp/codefresh \
+            && cd /tmp/codefresh \
+            && CODEFRESH_URL=$(curl -s https://api.github.com/repos/codefresh-io/cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux-x64")) | .browser_download_url') \
+            && curl -sLJ "$CODEFRESH_URL" -o "codefresh.tar.gz" \
+            && tar xzf codefresh.tar.gz \
+            && sudo mv ./codefresh /usr/local/bin \
+            && sudo chmod 755 /usr/local/bin/codefresh
+    )
+    rm -rf /tmp/codefresh
+}
+
+update_pulumi() {
+    (
+        mkdir /tmp/pulumi/ \
+            && cd /tmp/pulumi \
+            && PULUMI_URL=$(curl -s https://api.github.com/repos/pulumi/pulumi/releases/latest | jq -r '.assets[] | select(.name | contains("linux-x64")) | .browser_download_url') \
+            && curl -sLJ "$PULUMI_URL" -o "pulumi.tar.gz" \
+            && tar xzf pulumi.tar.gz \
+            && sudo mv pulumi/pulumi* /usr/local/bin \
+            && sudo chmod 755 /usr/local/bin/pulumi*
+    )
+    rm -rf /tmp/pulumi
+}
+
+update_github() {
+    (
+        mkdir /tmp/github
+            && cd /tmp/github \
+            && GITHUB_URL=$(curl -s https://api.github.com/repos/cli/cli/releases/latest | jq -r '.assets[] | select(.name | contains("linux_amd64.tar.gz")) | .browser_download_url') \
+            && curl -LJs "$GITHUB_URL" -o "github.tar.gz" \
+            && tar xzf "github.tar.gz" \
+            && sudo mv gh_*_linux_amd64/bin/gh /usr/local/bin/gh \
+            && sudo chmod +x /usr/local/bin/gh
+    )
+    rm -rf /tmp/github
+}
+
