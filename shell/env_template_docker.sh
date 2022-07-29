648acf18bc8fd18cf1c5b3722ee812dec96e2f45
Chris Roselle <chrisroselle@gmail.com>
2022-07-29 05:29:26 -0400

    add new variable

--- shell/env_template_docker.sh
+++ shell/env_template_docker.sh
@@ -9,5 +9,6 @@ export HISTCONTROL="erasedups"
 
 #export CLI_EDITOR="vi"
 export CLI_TOOLS='python yq kubectl helm node maven aws codefresh pulumi github ripgrep gron'
+export DEFAULT_KUBE_CONTEXT='default'
 
 python "$_DIR/script/dedup.py" ~/.bash_history
--- shell/env_template_wsl.sh
+++ shell/env_template_wsl.sh
@@ -11,6 +11,7 @@ export HISTCONTROL="erasedups"
 export BROWSER='/mnt/c/Program Files/Mozilla Firefox/firefox.exe'
 export CLI_EDITOR='np'
 export CLI_TOOLS='python yq kubectl helm node maven aws codefresh pulumi github ripgrep gron'
+export DEFAULT_KUBE_CONTEXT='default'
 export WIN_USERNAME=chris
 
 python "$_DIR/script/dedup.py" ~/.bash_history
